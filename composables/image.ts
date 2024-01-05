5415b3096812c6973bd2632fcfd0414cc1d7d86e
Abdelrahman Khalil <samyz.k71@gmail.com>
2024-01-05 20:15:27 +0200

    refactor dashboard, add search and refresh

--- composables/image.ts
+++ composables/image.ts
@@ -1,5 +1,3 @@
-import type { PaginationState } from '@tanstack/vue-table'
-
 export async function dbLockImage(id: string) {
   const supabase = useSupabase()
 
@@ -74,95 +72,18 @@ export function dbGetUniqueImage() {
     .then(({ data }) => data)
 }
 
-// export function dbGetImagesCount() {
-//   const supabase = useSupabase()
-
-//   return supabase
-//     .from('stats')
-//     .select('images')
-//     .single()
-//     .then(({ data }) => data?.images)
-// }
-
-export async function useImages(page: number, perPage: number) {
-  const supabase = useSupabase()
-
-  const isLoading = ref(false)
-
-  function getImages(
-    state = {
-      currentPage: page,
-      currentPageSize: perPage,
-    },
-  ) {
-    return supabase
-      .from('images')
-      .select()
-      .range(
-        (state.currentPage - 1) * state.currentPageSize,
-        state.currentPage * state.currentPageSize - 1,
-      )
-      .order('word')
-      .then(({ data }) => {
-        isLoading.value = false
-
-        return data
-      })
-  }
-
-  const debouncedGetImage = useDebounceFn(getImages, 500)
-
-  const images = ref(await debouncedGetImage())
-
-  const pagination = useOffsetPagination({
-    async onPageChange(state) {
-      isLoading.value = true
-      const data = await debouncedGetImage(state)
-
-      if (data) {
-        images.value = data
-      }
-    },
-    page,
-    pageSize: perPage,
-    async onPageCountChange(state) {
-      isLoading.value = true
-      const data = await debouncedGetImage(state)
-
-      if (data) {
-        images.value = data
-      }
-    },
-  })
-
-  const currentPageSize = computed(() => images.value?.length || 0)
-
-  return {
-    isLoading,
-    images,
-    ...pagination,
-    currentPageSize,
-  }
-}
-
-export function dbGetImages(
-  page: number,
-  pageSize: number,
-  ignoreNullWords: boolean = false,
-  abortSignal: AbortSignal = new AbortController().signal,
-) {
+export function dbGetImages(page: number, pageSize: number, filterWord?: string) {
   const supabase = useSupabase()
 
   let query = supabase
     .from('images')
-    .select()
+    .select('*', { count: 'planned' })
     .range((page - 1) * pageSize, page * pageSize - 1)
     .order('word')
-    .abortSignal(abortSignal)
 
-  if (ignoreNullWords) query = query.not('word', 'is', null)
+  if (filterWord) query = query.filter('word', 'like', filterWord)
 
-  return query.then(({ data }) => data)
+  return query.then(({ data, count }) => ({ data, count }))
 }
 
 export function getImageUrl(file_name: string) {
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { imagesTableColumns } from './imagesTableColumns'
 import { contributorsTableColumns } from './contributorsTableColumns'
+import { useRouteQuery } from '@vueuse/router'
 
 definePageMeta({
   middleware: 'admin',
@@ -12,54 +13,56 @@ useHead({
 
 const IMAGE_PAGE_SIZE = 100
 
-const route = useRoute('dashboard')
-const page = (route.query.page as unknown as number) || 1
+const page = useRouteQuery('page', 1, { transform: Number })
+const filterWord = useRouteQuery('word', '')
+const filterWordDeb = refDebounced(filterWord, 1000)
 
-const { data, refresh } = await useAsyncData('dashboard', () => {
-  return Promise.all([dbGetStats(), dbGetUsers(), dbGetImages(page, IMAGE_PAGE_SIZE)])
-})
+syncRef(filterWord, filterWordDeb)
 
-const stats = computed(() => data.value?.[0])
-const users = computed(() => data.value?.[1])
-const images = ref(data.value?.[2])
+const [stats, users, images] = await Promise.all([
+  useAsyncData('stats', async () => await dbGetStats()),
+  useAsyncData('users', async () => await dbGetUsers()),
+  useAsyncData(
+    `images:${page.value}`,
+    async () => await dbGetImages(page.value, IMAGE_PAGE_SIZE, filterWordDeb.value),
+    {
+      watch: [page, filterWordDeb],
+    },
+  ),
+])
 
-const isLoading = ref(false)
-const isLive = ref(false)
-
-let ac: AbortController
+const statsData = stats.data
+const usersData = users.data
+const imagesData = images.data
+const imagesCount = computed(() => images.data.value?.count || 0)
 
 const { currentPage, pageCount, currentPageSize, isFirstPage, isLastPage, next, prev } =
   useOffsetPagination({
-    page,
+    page: page,
     pageSize: IMAGE_PAGE_SIZE,
-    total: stats?.value?.images || 0,
-    onPageChange: async state => {
-      if (ac) ac.abort()
-      ac = new AbortController()
-
-      isLoading.value = true
-      const data = await dbGetImages(
-        state.currentPage,
-        state.currentPageSize,
-        isLive.value,
-        ac.signal,
-      )
-
-      if (data) {
-        isLoading.value = false
-        images.value = data
-
-        const pageQuery = state.currentPage === 1 ? '' : `?page=${state.currentPage}`
-        navigateTo('/dashboard' + pageQuery)
-      }
-    },
+    total: imagesCount,
   })
 
-const live = useIntervalFn(refresh, 5000)
+function refresh() {
+  stats.refresh()
+  users.refresh()
+  images.refresh()
+}
 
-watch(isLive, isLive => {
-  isLive ? live.resume() : live.pause()
+const stream = useIntervalFn(refresh, 5000, {
+  immediate: false,
 })
+
+const isLive = ref(false)
+watch(
+  isLive,
+  live => {
+    live ? stream.resume() : stream.pause()
+  },
+  {
+    immediate: true,
+  },
+)
 </script>
 
 <template>
@@ -69,23 +72,32 @@ watch(isLive, isLive => {
         <TagCard
           icon="tabler:tag"
           title="الكلمات"
-          :value="stats?.words"
-          :of="stats?.images"
+          :value="statsData?.words"
+          :of="statsData?.images"
         />
-        <TagCard icon="tabler:users" title="المساهمون" :value="stats?.contributors" />
+        <TagCard
+          icon="tabler:users"
+          title="المساهمون"
+          :value="statsData?.contributors"
+        />
       </div>
 
-      <DataTable :columns="contributorsTableColumns" :data="users" />
+      <DataTable
+        :columns="contributorsTableColumns"
+        :data="usersData"
+        :loading="users.pending.value"
+      />
     </div>
 
     <div class="flex w-full flex-col gap-4">
       <div class="flex justify-between">
         <div class="flex w-5/12 gap-2">
-          <Input placeholder="بحث..." />
-          <Toggle variant="outline" color="destructive" v-model:pressed="isLive">
+          <Input placeholder="بحث..." v-model:model-value="filterWordDeb" />
+          <!-- <Toggle variant="outline" color="destructive" v-model:pressed="isLive">
             مباشر
             <Icon name="tabler:point-filled" class="ms-2 w-[16px]" />
-          </Toggle>
+          </Toggle> -->
+          <Button variant="outline" @click="refresh">تحديث</Button>
         </div>
 
         <!-- <Button variant="secondary">
@@ -97,8 +109,8 @@ watch(isLive, isLive => {
       <DataTable
         class="h-full"
         :columns="imagesTableColumns"
-        :data="images"
-        :loading="isLoading"
+        :data="imagesData?.data"
+        :loading="images.pending.value"
       />
 
       <div class="flex justify-between">
@@ -120,7 +132,9 @@ watch(isLive, isLive => {
           <Button variant="outline" :disabled="isFirstPage" @click="prev">
             السابق
           </Button>
-          <Button variant="outline" :disabled="isLastPage" @click="next">التالى</Button>
+          <Button variant="outline" :disabled="isLastPage" @click="next">
+            التالى
+          </Button>
         </div>
       </div>
     </div>
