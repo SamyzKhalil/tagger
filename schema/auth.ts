87edb2460e7dc71c4486f873e95b57a057bcb57b
Abdelrahman Khalil <samyz.k71@gmail.com>
2024-01-09 23:57:23 +0200

    better auth errors

--- schema/auth.ts
+++ schema/auth.ts
@@ -1,9 +1,19 @@
-import { type Output, object, string, minLength, maxLength } from 'valibot'
+import {
+  type Output,
+  object,
+  string,
+  minLength,
+  maxLength,
+  excludes,
+  regex,
+} from 'valibot'
 
 export const formSchema = object({
   handle: string('هذا الحقل مطلوب', [
     minLength(3, 'يجب ألا يقل المُعرّف عن 3 رموز'),
-    maxLength(16, 'يجب ألا يزيد المُعرّف عن 16 رمزاً'),
+    maxLength(32, 'يجب ألا يزيد المُعرّف عن 32 رمزاً'),
+    excludes(' ', 'يجب ألا يحتوى المُعرّف على مسافات'),
+    regex(/\w+/, 'يجب أن يحتوى المُعرّف على أحرف إنجليزية وأرقام فقط'),
   ]),
   password: string('هذا الحقل مطلوب', [
     minLength(6, 'يجب ألا تقل كلمة المرور عن 6 رموز'),
